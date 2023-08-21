ec012738ba2aab822a6af04c2cb27229a9ff2023
Rogelio Gudino <cananito@gmail.com>
2023-08-21 11:42:15 -0700

    No need to set visibility of JS. All is run from JS.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-// Public
-
 function rg_buttonWasClicked(element) {
   console.log("Element clicked: " + element.id + " - " + element.innerHTML);
   var soundFilePath = "TODO"
   _rg_play(soundFilePath);
 };
 
-// Private
-
-function _rg_play(soundFilePath) {
+function rg_play(soundFilePath) {
   console.log("Sound file path: " + soundFilePath);
 };
 
-function _rg_setUpDOM() {
+function rg_setUpDOM() {
   // TODO: Clean this up to avoid repeating so much code.
 
   const buttonGridDiv = document.createElement("div");
