33f16a6157acb8a82deb61b2a15032b53deca773
Rogelio Gudino <cananito@gmail.com>
2023-09-20 16:56:33 -0700

    Added example of how to test, but won't ever use it.

---
+++ js/tests/main_tests.js
@@ -0,0 +1,9 @@
+// TODO!
+
+function runTests() {
+  const span = document.createElement("span");
+  span.innerHTML = "TODO!!!!!!!!!!";
+  document.body.appendChild(span);
+};
+
+document.addEventListener("DOMContentLoaded", runTests);
